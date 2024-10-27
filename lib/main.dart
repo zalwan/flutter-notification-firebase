e35dcf5ec4568163bf081c9a03034864d66eb608
rizal suryawan <rizalsuryawan86@gmail.com>
2024-10-27 15:28:18 +0700

    "Refactor main.dart to handle Firebase messaging and notifications, and remove notification_storage_service.dart"

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,112 @@
-// lib/main.dart
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:notification_sample/firebase_options.dart';
 import 'package:notification_sample/presentation/home/pages/home_page.dart';
-import 'package:notification_sample/services/firebase/firebase_messaging_service.dart';
+import 'package:notification_sample/presentation/notification/pages/notification_page.dart';
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await setupFlutterNotifications();
+  showFlutterNotification(message);
+
+  print('Handling a background message title: ${message.notification?.title}');
+  print('Handling a background message body: ${message.notification?.body}');
+}
+
+late AndroidNotificationChannel channel;
+late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+bool isFlutterLocalNotificationsInitialized = false;
+
+Future<void> setupFlutterNotifications() async {
+  if (isFlutterLocalNotificationsInitialized) {
+    return;
+  }
+  channel = const AndroidNotificationChannel(
+    'high_importance_channel',
+    'High Importance Notifications',
+    description: 'This channel is used for important notifications.',
+    importance: Importance.high,
+  );
+
+  flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+  isFlutterLocalNotificationsInitialized = true;
+}
+
+void showFlutterNotification(RemoteMessage message) {
+  RemoteNotification? notification = message.notification;
+  AndroidNotification? android = message.notification?.android;
+  if (notification != null && android != null && !kIsWeb) {
+    flutterLocalNotificationsPlugin.show(
+      notification.hashCode,
+      notification.title,
+      notification.body,
+      NotificationDetails(
+        android: AndroidNotificationDetails(
+          channel.id,
+          channel.name,
+          channelDescription: channel.description,
+          icon: 'launch_background',
+        ),
+      ),
+    );
+  }
+}
+
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  FirebaseMessagingService firebaseMessagingService =
-      FirebaseMessagingService();
-  await firebaseMessagingService.initialize();
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print('FCM token: $fcmToken');
+
+  // set notification on background
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  if (!kIsWeb) {
+    await setupFlutterNotifications();
+  }
+
+  // set notification on terminate
+  FirebaseMessaging.instance.getInitialMessage().then((message) {
+    if (message != null) {
+      navigatorKey.currentState?.push(
+        MaterialPageRoute(builder: (context) => const NotificationPage()),
+      );
+    }
+  });
+
+  // set notification on foreground
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    showFlutterNotification(message);
+    print(
+        'Handling a foreground message title: ${message.notification?.title}');
+    print('Handling a foreground message body: ${message.notification?.body}');
+  });
+
+  // set notification on open app
+  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+    navigatorKey.currentState?.push(
+      MaterialPageRoute(builder: (context) => const NotificationPage()),
+    );
+  });
 
   runApp(const MyApp());
 }
@@ -22,6 +117,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: navigatorKey, // Set navigatorKey untuk navigasi global
       title: 'FCM Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
