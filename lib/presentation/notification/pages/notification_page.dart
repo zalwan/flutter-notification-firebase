ff95d94abe3435e68166e5bd7134d179d952cd42
rizal suryawan <rizalsuryawan86@gmail.com>
2024-10-30 13:21:06 +0700

    "Refactor NotificationPage from StatelessWidget to StatefulWidget, add delete functionality with checkbox selection and bottom sheet menu"

--- lib/presentation/notification/pages/notification_page.dart
+++ lib/presentation/notification/pages/notification_page.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:notification_sample/presentation/notification/bloc/notification_bloc.dart';
 
-class NotificationPage extends StatelessWidget {
+class NotificationPage extends StatefulWidget {
   const NotificationPage({super.key});
 
   static const route = '/notification-page';
+
+  @override
+  _NotificationPageState createState() => _NotificationPageState();
+}
+
+class _NotificationPageState extends State<NotificationPage> {
+  List<String> selectedNotificationIds = [];
+  bool isDeleting = false;
 
   @override
   Widget build(BuildContext context) {
@@ -18,20 +27,15 @@ class NotificationPage extends StatelessWidget {
         title: const Text('Notification History'),
         actions: [
           IconButton(
-            icon: const Icon(Icons.delete),
-            tooltip: 'Delete All Notifications',
+            icon: const Icon(Icons.more_vert),
             onPressed: () {
-              context
-                  .read<NotificationBloc>()
-                  .add(const NotificationEvent.deleteAll());
+              _showBottomDialog(context);
             },
           ),
         ],
       ),
       body: BlocBuilder<NotificationBloc, NotificationState>(
         builder: (context, state) {
-          // print('Current NotificationState: $state');
-
           return state.map(
             initial: (_) =>
                 const Center(child: Text('Welcome to Notifications')),
@@ -43,19 +47,69 @@ class NotificationPage extends StatelessWidget {
                 itemBuilder: (context, index) {
                   final notification = notifications[index];
                   return ListTile(
+                    leading: const Icon(Icons.notifications_active),
                     tileColor:
-                        notification.isRead ? Colors.white : Colors.grey[300],
-                    title: Text(notification.title),
-                    subtitle: Text(notification.body),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.delete),
-                      onPressed: () {
-                        context.read<NotificationBloc>().add(
-                              NotificationEvent.deleteNotification(
-                                  notification.id),
-                            );
-                      },
+                        notification.isRead ? Colors.white : Colors.grey[200],
+                    title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Info',
+                              style: TextStyle(
+                                color: Colors.grey[600],
+                                fontSize: 13,
+                              ),
+                            ),
+                            Text(
+                              DateFormat('dd MMM yyyy • HH:mm')
+                                  .format(notification.timestamp),
+                              style: TextStyle(
+                                color: Colors.grey[600],
+                                fontSize: 13,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Text(
+                          notification.title,
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
                     ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 2),
+                        Text(notification.body),
+                        const SizedBox(height: 8),
+                        const Divider(
+                          height: 0,
+                          thickness: 0,
+                          indent: 0,
+                          endIndent: 0,
+                          color: Colors.grey,
+                        ),
+                      ],
+                    ),
+                    trailing: isDeleting
+                        ? Checkbox(
+                            value: selectedNotificationIds
+                                .contains(notification.id),
+                            onChanged: (bool? value) {
+                              setState(() {
+                                if (value == true) {
+                                  selectedNotificationIds.add(notification.id);
+                                } else {
+                                  selectedNotificationIds
+                                      .remove(notification.id);
+                                }
+                              });
+                            },
+                          )
+                        : null,
                     onTap: () {
                       if (!notification.isRead) {
                         context.read<NotificationBloc>().add(
@@ -71,6 +125,65 @@ class NotificationPage extends StatelessWidget {
           );
         },
       ),
+      floatingActionButton: isDeleting && selectedNotificationIds.isNotEmpty
+          ? FloatingActionButton(
+              onPressed: () {
+                for (var id in selectedNotificationIds) {
+                  context.read<NotificationBloc>().add(
+                        NotificationEvent.deleteNotification(id),
+                      );
+                }
+                setState(() {
+                  selectedNotificationIds.clear();
+                  isDeleting = false;
+                });
+              },
+              child: const Icon(Icons.delete),
+              tooltip: 'Delete Selected Notifications',
+            )
+          : null,
+    );
+  }
+
+  void _showBottomDialog(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                'Delete Menu',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              ListTile(
+                leading: const Icon(Icons.delete),
+                title: const Text('Select Notifications to Delete'),
+                onTap: () {
+                  setState(() {
+                    isDeleting = true;
+                  });
+                  Navigator.pop(context);
+                },
+              ),
+              const SizedBox(height: 10),
+              ListTile(
+                leading: const Icon(Icons.delete_forever),
+                title: const Text('Delete All Notifications'),
+                onTap: () {
+                  context
+                      .read<NotificationBloc>()
+                      .add(const NotificationEvent.deleteAll());
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
